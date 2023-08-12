101a96deefb23701991b9b7a070b5985c0976ed0
Andrii Zymohliad <andrii.zymohliad@lightcodephotonics.com>
2023-08-12 13:42:39 +0300

    Autoformat

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,7 @@
-use std::{io, sync::mpsc, thread, time};
 use anyhow::Result;
 use clap::Parser;
 use evdev::{self, uinput};
-
+use std::{io, sync::mpsc, thread, time};
 
 /// Adds inertia to your touchpad
 ///
@@ -38,12 +37,10 @@ struct Args {
     multitouch_cooldown: u64,
 }
 
-
 enum MomentumMessage {
     StartMovement(f64, f64),
     StopMovement,
 }
-
 
 /// Emulates mouse device (via uinput) which performs inertial pointer movement
 struct VirtualMouse {
@@ -54,25 +51,46 @@ impl VirtualMouse {
     fn new() -> Result<Self> {
         let device = uinput::VirtualDeviceBuilder::new()?
             .name("InertPad Virtual Mouse")
-            .input_id(evdev::InputId::new(evdev::BusType::BUS_USB, 0x1234, 0x5678, 0))
+            .input_id(evdev::InputId::new(
+                evdev::BusType::BUS_USB,
+                0x1234,
+                0x5678,
+                0,
+            ))
             .with_keys(
-                &[
-                    evdev::Key::BTN_LEFT
-                ].into_iter().collect::<evdev::AttributeSet<_>>())?
+                &[evdev::Key::BTN_LEFT]
+                    .into_iter()
+                    .collect::<evdev::AttributeSet<_>>(),
+            )?
             .with_relative_axes(
                 &[
                     evdev::RelativeAxisType::REL_X,
                     evdev::RelativeAxisType::REL_Y,
-                ].into_iter().collect::<evdev::AttributeSet<_>>())?
+                ]
+                .into_iter()
+                .collect::<evdev::AttributeSet<_>>(),
+            )?
             .build()?;
         Ok(Self { device })
     }
 
-    fn set_position(&mut self, x: i32, y: i32) -> io::Result<()>{
+    fn set_position(&mut self, x: i32, y: i32) -> io::Result<()> {
         let events = [
-            evdev::InputEvent::new(evdev::EventType::RELATIVE, evdev::RelativeAxisType::REL_X.0, x),
-            evdev::InputEvent::new(evdev::EventType::RELATIVE, evdev::RelativeAxisType::REL_Y.0, y),
-            evdev::InputEvent::new(evdev::EventType::SYNCHRONIZATION, evdev::Synchronization::SYN_REPORT.0, 0),
+            evdev::InputEvent::new(
+                evdev::EventType::RELATIVE,
+                evdev::RelativeAxisType::REL_X.0,
+                x,
+            ),
+            evdev::InputEvent::new(
+                evdev::EventType::RELATIVE,
+                evdev::RelativeAxisType::REL_Y.0,
+                y,
+            ),
+            evdev::InputEvent::new(
+                evdev::EventType::SYNCHRONIZATION,
+                evdev::Synchronization::SYN_REPORT.0,
+                0,
+            ),
         ];
         self.device.emit(&events)?;
         Ok(())
@@ -83,7 +101,7 @@ impl VirtualMouse {
         receiver: mpsc::Receiver<MomentumMessage>,
         drag: f64,
         speed_factor: f64,
-        refresh_rate: f64
+        refresh_rate: f64,
     ) {
         let period = time::Duration::from_secs_f64(refresh_rate.recip());
         let mut is_moving = false;
@@ -108,15 +126,17 @@ impl VirtualMouse {
                     }
                 }
             } else if let Ok(MomentumMessage::StartMovement(x, y)) = receiver.recv() {
-                log::debug!("Emulation: start movement, velocity = ({:.02}, {:.02})", x, y);
+                log::debug!(
+                    "Emulation: start movement, velocity = ({:.02}, {:.02})",
+                    x,
+                    y
+                );
                 is_moving = true;
                 (vx, vy) = (x, y);
             }
         }
     }
-
 }
-
 
 /// Captures raw evdev touchpad events and forwards
 struct Touchpad {
@@ -128,7 +148,8 @@ impl Touchpad {
         for (_path, device) in evdev::enumerate() {
             if let Some(keys) = device.supported_keys() {
                 if keys.contains(evdev::Key::BTN_TOOL_FINGER)
-                && keys.contains(evdev::Key::BTN_TOUCH) {
+                    && keys.contains(evdev::Key::BTN_TOUCH)
+                {
                     return Some(Self { device });
                 }
             }
@@ -140,11 +161,11 @@ impl Touchpad {
         &mut self,
         sender: mpsc::Sender<MomentumMessage>,
         speed_threshold: f64,
-        multitouch_cooldown: u64)
-    {
+        multitouch_cooldown: u64,
+    ) {
         let (mut vx, mut vy) = (0f64, 0f64);
         let (mut x, mut y) = (0, 0);
-        let (mut prev_x, mut prev_y)  = (0, 0);
+        let (mut prev_x, mut prev_y) = (0, 0);
         let mut timestamp = time::SystemTime::UNIX_EPOCH;
         let mut prev_timestamp = time::SystemTime::UNIX_EPOCH;
         let mut multitouch_timestamp = time::SystemTime::UNIX_EPOCH;
@@ -159,39 +180,50 @@ impl Touchpad {
                         evdev::AbsoluteAxisType::ABS_X => x = event.value(),
                         evdev::AbsoluteAxisType::ABS_Y => y = event.value(),
                         _ => (),
-                    }
+                    },
                     evdev::InputEventKind::Key(key) => match key {
-                        evdev::Key::BTN_TOOL_FINGER => if event.value() == 1 {
-                            let _ = sender.send(MomentumMessage::StopMovement);
-                            (vx, vy) = (0.0, 0.0);
-                            (prev_x, prev_y) = (x, y); // Prevent velocity overwrite later
-                        } else {
-                            // Filter out multi-touch lift-off
-                            if timestamp.duration_since(multitouch_timestamp).unwrap_or_default() < multitouch_cooldown {
-                                continue;
+                        evdev::Key::BTN_TOOL_FINGER => {
+                            if event.value() == 1 {
+                                let _ = sender.send(MomentumMessage::StopMovement);
+                                (vx, vy) = (0.0, 0.0);
+                                (prev_x, prev_y) = (x, y); // Prevent velocity overwrite later
+                            } else {
+                                // Filter out multi-touch lift-off
+                                if timestamp
+                                    .duration_since(multitouch_timestamp)
+                                    .unwrap_or_default()
+                                    < multitouch_cooldown
+                                {
+                                    continue;
+                                }
+                                let speed = (vx * vx + vy * vy).sqrt();
+                                if speed >= speed_threshold {
+                                    let _ = sender.send(MomentumMessage::StartMovement(vx, vy));
+                                }
                             }
-                            let speed = (vx * vx + vy * vy).sqrt();
-                            if speed >= speed_threshold {
-                                let _ = sender.send(MomentumMessage::StartMovement(vx, vy));
+                        }
+                        evdev::Key::BTN_TOOL_DOUBLETAP
+                        | evdev::Key::BTN_TOOL_TRIPLETAP
+                        | evdev::Key::BTN_TOOL_QUADTAP
+                        | evdev::Key::BTN_TOOL_QUINTTAP => {
+                            if event.value() == 1 {
+                                let _ = sender.send(MomentumMessage::StopMovement);
+                            } else {
+                                multitouch_timestamp = timestamp;
                             }
-                        },
-                        evdev::Key::BTN_TOOL_DOUBLETAP |
-                        evdev::Key::BTN_TOOL_TRIPLETAP |
-                        evdev::Key::BTN_TOOL_QUADTAP |
-                        evdev::Key::BTN_TOOL_QUINTTAP => if event.value() == 1 {
-                            let _ = sender.send(MomentumMessage::StopMovement);
-                        } else {
-                            multitouch_timestamp = timestamp;
                         }
                         _ => {}
-                    }
+                    },
                     _ => {}
                 }
             }
             if x != prev_x || y != prev_y {
                 let dx = (x - prev_x) as f64;
                 let dy = (y - prev_y) as f64;
-                let dt = timestamp.duration_since(prev_timestamp).unwrap().as_secs_f64();
+                let dt = timestamp
+                    .duration_since(prev_timestamp)
+                    .unwrap()
+                    .as_secs_f64();
                 (vx, vy) = (dx / dt, dy / dt);
                 (prev_x, prev_y) = (x, y);
                 prev_timestamp = timestamp;
@@ -200,7 +232,6 @@ impl Touchpad {
         }
     }
 }
-
 
 fn main() {
     env_logger::Builder::new()
@@ -214,13 +245,20 @@ fn main() {
     match Touchpad::default() {
         None => log::error!("Touchpad not found!"),
         Some(mut touchpad) => {
-            log::info!("Found touchpad: {}", touchpad.device.name().unwrap_or_default());
+            log::info!(
+                "Found touchpad: {}",
+                touchpad.device.name().unwrap_or_default()
+            );
             match VirtualMouse::new() {
                 Err(e) => log::error!("Failed to create virtual mouse device: {}", e),
                 Ok(mut vmouse) => {
                     log::info!("Virtual mouse device is created");
                     thread::spawn(move || {
-                        touchpad.run_capture(sender, args.speed_threshold, args.multitouch_cooldown);
+                        touchpad.run_capture(
+                            sender,
+                            args.speed_threshold,
+                            args.multitouch_cooldown,
+                        );
                     });
                     vmouse.run_emulation(receiver, args.drag, args.speed_factor, args.refresh_rate);
                 }
